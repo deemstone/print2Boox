eb9ba05ea4c9ef458d0cf3b219fd07cb600f5be0
deemstone <deemstone@gmail.com>
2019-05-30 22:39:20 +0800

    临时兼容send2boox新版api

--- background.js
+++ background.js
@@ -36,17 +36,36 @@ const Util = {
 };
 
 window.stss = null;
+window.auth = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjA0ODMsInd4VW5pb25pZCI6Im9JdWJZdm42SkRsUWdLVEQwc0ZEOGdDVzFZVGsiLCJpYXQiOjE1NTg5NjgzMDEsImV4cCI6MTU2MTU2MDMwMX0.9xhHHf3yn1DtJ-aJrJ7-oaf3rypVXwru9_il0Ij8Z2Y';
 
 var CAPABILITIES = ' {      "version": "1.0",      "printer": {       "supported_content_type": [          {"content_type": "application/pdf","min_version":"1.5","max_version":"1.5"},          {"content_type": "image/pwg-raster"}        ],        "copies": {          "default": 1,          "max": 100        },        "color":{          "option": [            {"type":  "STANDARD_COLOR", "is_default":  true},            {"type":  "STANDARD_MONOCHROME"}          ]        },        "duplex":{          "option": [            {"type": "NO_DUPLEX", "is_default":  true },            {"type": "LONG_EDGE"},            {"type": "SHORT_EDGE"}          ]        },        "collate": { "default": true},        "page_orientation":{"option":[{"type":"PORTRAIT","is_default":true},{"type":"LANDSCAPE"}]},        "margins":{"option":[{"type":"BORDERLESS","top":0,"left":0,"right":0,"bottom":0},{"type":"STANDARD","is_default":true},{"type":"CUSTOM"}]},        "media_size": {      "option": [        {          "name": "ISO_A4",          "width_microns": 210000,          "height_microns": 297000,          "is_default": false        },         {          "name": "ISO_A5",          "width_microns": 105000,          "height_microns": 148500,          "is_default": false        },        {          "name": "NA_INDEX_4X6",          "width_microns": 100000,          "height_microns": 150000        },        {          "name": "NA_LETTER",          "width_microns": 215900,          "height_microns": 279400,          "is_default": true        }      ]    }      }    } ';
 const cdd = JSON.parse(CAPABILITIES);
 
 chrome.printerProvider.onGetPrintersRequested.addListener(function(e) {
 	var r = [];
-	r.push({
-		id: "Print to Boox",
-		name: "Print to Boox"
-	});
-	e(r);
+
+    if(!window.auth ){
+        //TODO:
+        chrome.notifications.create('onGetPrintersRequested', {
+            type: "basic",
+            iconUrl: "images/get_started32.png",
+            title: "Print message",
+            message: "请先登录send2Boox",
+            //expandedMessage: "",
+            priority: 1,
+            //buttons: [{
+            //    title: "go!"
+            //},{
+            //    title: "abort"
+            //}]
+        });
+    }else{
+        r.push({
+            id: "Print to Boox",
+            name: "Print to Boox"
+        });
+        e(r);
+    }
 });
 
 chrome.printerProvider.onGetCapabilityRequested.addListener(function(e, r) {
@@ -68,27 +87,34 @@ chrome.printerProvider.onPrintRequested.addListener(function(e, r) {
     });
 });
 
-const API_PREFIX = 'http://send2boox.com/api';
+const API_PREFIX = 'http://send2boox.com/api/1';
 
 window.ossClient = null;  // client放到全局缓存
 async function getOssClient() {
     if( !window.ossClient ){
-        const creds = await fetch(`${API_PREFIX}/stss`, {
-            method: 'get'
-        }).then(response => response.json());
-
         const bucket = 'onyx-content';
         const region = 'oss-cn-shenzhen';
 
         const client = new OSS({
             region: region,
-            accessKeyId: creds.AccessKeyId,
-            accessKeySecret: creds.AccessKeySecret,
-            stsToken: creds.SecurityToken,
+            accessKeyId: '__fake__', // creds.AccessKeyId,
+            accessKeySecret: '__fake__', // creds.AccessKeySecret,
+            stsToken: '__fake__', // creds.SecurityToken,
             bucket: bucket
         });
         window.ossClient = client;
+
     }
+
+    // 因为有效期比auth短 每次都更新stsToken
+    const { data: creds } = await fetch(`${API_PREFIX}/stss/`, {
+        method: 'get'
+    }).then(response => response.json());
+
+    window.ossClient.options.accessKeyId = creds.AccessKeyId;
+    window.ossClient.options.accessKeySecret = creds.AccessKeySecret;
+    window.ossClient.options.stsToken = creds.SecurityToken;
+
     return window.ossClient;
 }
 
@@ -140,12 +166,13 @@ async function pushFile(task) {
             data: data,
             type: 'digital_content'
         };
-        return await fetch(`${API_PREFIX}/1/push/saveAndPush`,{
+        return await fetch(`${API_PREFIX}/push/saveAndPush`,{
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
             headers: {
                 'Content-Type': 'application/json',
                 // 'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': window.auth,
             },
             redirect: 'follow', // manual, *follow, error
             // referrer: 'no-referrer', // no-referrer, *client
